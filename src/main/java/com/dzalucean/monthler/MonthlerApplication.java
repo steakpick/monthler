c817975113419fc73bffac1063663dec14a46bd7
Dumitru Zalucean <dumitruzalucean@MacBook-Pro-Dumitru.local>
2024-01-01 18:00:03 +0200

    initial commit

---
+++ src/main/java/com/dzalucean/monthler/MonthlerApplication.java
@@ -0,0 +1,13 @@
+package com.dzalucean.monthler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dzalucean/monthler/MonthlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzalucean.monthler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
